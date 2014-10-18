5dd4dbb2c88950f41147ed1a4cf110cf09c27bee
Maria Nita <maria.nita.dn@gmail.com>
2014-10-18 14:16:08 +0200

    Add fibonacci example

---
+++ fibonacci.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+    a := 0
+    b := 1
+    c := 0
+    
+    return func() int {
+        c = a
+        a = a + b
+        b = c
+        return c
+    }
+}
+
+func main() {
+    f := fibonacci()
+    for i := 0; i < 10; i++ {
+        fmt.Println(f())
+    }
+}
