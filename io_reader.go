37318b32412de4708341dd8b57adcb5a49039454
Maria Nita <maria.nita.dn@gmail.com>
2014-10-24 11:30:17 +0200

    Add ioReader example

---
+++ io_reader.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "io"
+    "os"
+    "strings"
+)
+
+type rot13Reader struct {
+    r io.Reader
+}
+
+func (rot *rot13Reader) Read(p []byte) (n int, e error) {
+    return rot.r.Read(p)
+}
+
+func main() {
+    s := strings.NewReader("Lbh penpxrq gur pbqr!")
+    r := rot13Reader{s}
+    io.Copy(os.Stdout, &r)
+}
